16e657590458f0b9ca3e14622fc3f8f0bad16d63
Jamie Naylor <jamie@naylorwebsites.co.uk>
2018-11-26 17:43:06 +0000

    added wasteland junk skill

---
+++ wasteland junk/recipes.js
@@ -0,0 +1,26 @@
+/* eslint quote-props: ['error', 'consistent'] */
+module.exports = {
+    'junk': {
+        'adhesive':'duct tape, wonderglue',
+        'aluminum	':'aluminum cans, alarm clocks, coolant caps, sensors, trays toy rocket ships',
+        'Bone':'Rib cage and spine, etc.',
+        'ceramic':'Willow flared vase',
+        'circuitry':'circuit boards, hot plates, targeting cards, telephones',
+        'crystal':'Cameras, crystal liquor decanters, microscopes',
+        'fertilizer':'	Spoiled meat, spoiled fruit, spoiled vegetables',
+        'fiber optics':'biometric scanners, microscopes',
+        'fiberglass':'	Abraxo cleaner, cigar boxes, telephones',
+        'gears':'	Adjustable wrenches, cameras, desk fans, fishing rods, typewriters, watches',
+        '':'',
+        '':'',
+        '':'',
+        '':'',
+        '':'',
+        '':'',
+        '':'',
+        '':'',
+        '':'',
+  
+        
+    }
+  };
